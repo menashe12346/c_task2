f83357cfd129d83f2650f7e21699516bd6c22581
menashe12346 <menashe390@gmail.com>
2024-02-13 11:08:13 +0200

    ds

--- my_Knapsack.c
+++ my_Knapsack.c
@@ -37,10 +37,10 @@ int knapSack(int weights[], int values[], int selected_bool[]) {
 int main() {
     int values[size];
     int weights[size];
-    char items[] = {'a', 'b', 'c', 'd', 'e'};
+    char items[size];
 
-    for (int i = 0; i < 5; i++) {
-        scanf("%c", &items[i]);
+    for (int i = 0; i < size; i++) {
+        scanf(" %c", &items[i]);
         scanf("%d", &values[i]);
         scanf("%d", &weights[i]);
     }
--- my_mat.c
+++ my_mat.c
@@ -15,12 +15,13 @@ int min(int a, int b) {
 void A(int Array[ROWS][COLS]) {
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLS; j++) {
-            scanf("%d", &Array[i][j]);
+            scanf(" %d", &Array[i][j]);
             if (Array[i][j] == 0 && i != j) { 
                 Array[i][j] = INFINITY_VALUE;
             }
         }
     }
+                    scanf(" ");
     //make the shourtestPath array
     for (int k = 0; k < ROWS; k++) {
         for (int i = 0; i < ROWS; i++) {
